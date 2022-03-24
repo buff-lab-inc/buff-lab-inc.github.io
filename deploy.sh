bc39a95ddab427779e2ddce35dc7202bec9c978d
0woodev <0woodev@gmail.com>
2022-03-24 15:50:45 +0900

    쉘 스크립트 테스트

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
+echo "git pull 실행"
 git pull origin main
+echo "hexo clean 실행"
 hexo clean
+echo "hexo generate 실행"
 hexo generate
+echo "hexo deploy 실행"
 hexo deploy
+echo "git add 실행"
 git add .
+echo "git commit 실행"
 git commit
+echo "git commit 완료 후 push 실행"
 git push origin main
